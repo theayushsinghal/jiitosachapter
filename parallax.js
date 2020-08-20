f4a3911e2c9d76231a1d59fafccba5ac2a0d91a9
Ayush Singhal <55895435+aysinghal2001@users.noreply.github.com>
2020-08-20 22:57:29 +0530

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -1,6 +1,5 @@
 /**
  * Parallax.js
- * @author Matthew Wagerfield - @wagerfield
  * @description Creates a parallax effect between an array of layers,
  *              driving the motion from the gyroscope output of a smartdevice.
  *              If no gyroscope is available, the cursor position is used.
